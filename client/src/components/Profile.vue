a0f7231ea72a234f230925d88006ba662c19e9c6
Christian Cueni <christian.cueni@iterativ.ch>
2020-10-26 22:14:05 +0100

    Style profile form

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,14 +1,34 @@
 <template>
   <div class="profile">
     <h1 class="is-size-1">Edit Profile</h1>
+    <p>Duration {{ profileDuration }}</p>
     <!-- <form> -->
+      <div class="steps__title step-title container">
+        <div class="columns">
+          <div class="column is-one-quarter">
+            <h3>Step name</h3>
+          </div>
+          <div class="column is-one-quarter">
+            <h3>Duration</h3>
+          </div>
+          <div class="column is-one-quarter">
+            <h3>Temperature</h3>
+          </div>
+        </div>
+      </div>
       <ul id="steps">
         <li v-for="step in steps" :key="step.name">
           <Step v-bind="step" />
         </li>
         <Step @add-step="addStep" :editable="true" />
       </ul>
+      <div v-if="steps.length > 0">
+        <button class="button is-primary" >Start profile</button>
+      </div>
     <!-- </form> -->
+    <pre v-if="steps.length > 0">
+      {{ profileJson }}
+    </pre>
   </div>
 </template>
 
@@ -23,7 +43,13 @@ export default {
     Step
   },
   computed: {
-    ...mapGetters(['steps'])
+    ...mapGetters(['steps']),
+    profileDuration () {
+      return this.steps.reduce((duration, step) => step.duration + duration, 0)
+    },
+    profileJson () {
+      return JSON.stringify({ steps: this.steps })
+    }
   },
   methods: {
     ...mapActions(['addStep'])
--- client/src/components/Step.vue
+++ client/src/components/Step.vue
@@ -1,40 +1,41 @@
 <template>
-  <div class="step container">
-    <div class="columns">
-      <div class="column">
-        <div class="field">
-          <label class="label">Step name</label>
-          <div class="control">
-          <input class="input" type="text" placeholder="Text input" v-model="step.name">
+  <div class="steps">
+    <div class="step container">
+      <div class="columns">
+        <div class="column">
+          <div class="step-field">
+            <label class="step-field__label label">Step name</label>
+            <div class="control">
+            <input class="input" type="text" placeholder="Text input" v-model="step.name">
+            </div>
           </div>
         </div>
-      </div>
-      <div class="column">
-        <div class="field">
-          <label class="label">Duration</label>
-          <div class="control">
-          <input class="input" type="text" placeholder="Text input" v-model="step.duration">
+        <div class="column">
+          <div class="step-field">
+            <label class="step-field__label label">Duration</label>
+            <div class="control">
+            <input class="input" type="number" placeholder="Text input" v-model="step.duration">
+            </div>
           </div>
         </div>
-      </div>
-      <div class="column">
-        <div class="field">
-          <label class="label">Temperature</label>
-          <div class="control">
-          <input class="input" type="text" placeholder="Text input" v-model="step.temperature">
+        <div class="column">
+          <div class="step-field">
+            <label class="step-field__label label">Temperature</label>
+            <div class="control">
+            <input class="input" type="number" placeholder="Text input" v-model="step.temperature">
+            </div>
           </div>
         </div>
-      </div>
-      <div class="column">
-        <div v-if="editable">
-          <button class="button is-primary" @click="addStep">Add step</button>
-        </div>
-        <div v-else>
-          <button class="button is-primary" @click="addStep">Remove step</button>
+        <div class="column">
+          <div v-if="editable">
+            <button class="button is-primary" @click="addStep">Add step</button>
+          </div>
+          <div v-else>
+            <button class="button is-primary" @click="addStep">Remove step</button>
+          </div>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -73,7 +74,9 @@ export default {
   methods: {
     addStep () {
       this.$emit('add-step', {
-        ...this.step
+        temperature: Number(this.step.temperature),
+        duration: Number(this.step.duration),
+        name: this.step.name
       })
 
       this.step = {
@@ -85,3 +88,11 @@ export default {
   }
 }
 </script>
+<style scoped lang="scss">
+.step-field {
+  &__label {
+    display: none;
+  }
+}
+
+</style>
